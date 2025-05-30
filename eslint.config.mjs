a994dc1471369f94ba21b8b37db344cd73221587
Santiago Castillo <sacalla3@gmail.com>
2025-05-30 13:31:00 -0500

    edit in deployment ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"], // Aplica solo a archivos TS/TSX
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
