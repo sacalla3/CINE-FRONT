7a24e4006e630f98a9cecc6a98b976cd365275c2
ShammerY <samuel.ibarra1227@gmail.com>
2025-06-02 22:22:58 -0500

    feat: function list visual

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,9 +2,12 @@ import { Sidebar } from '../components/Sidebar';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex bg-black min-h-screen">
       <Sidebar />
-      <main className="pl-64 min-h-screen">{children}</main>
+      {/* <main className="pl-64 min-h-screen">{children}</main> */}
+      <main className="flex-1 min-h-screen p-6 pl-64">
+        {children}
+      </main>
     </div>
   );
 }
